0746f7a3b78815d351bb2dce7c8b6a27ba1f205a
PauloJf <PauloJf@users.noreply.github.com>
2015-10-14 11:36:19 +0100

    Update phonegap.js

--- phonegap.js
+++ phonegap.js
@@ -1,5 +1,8 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
+
+
+
 function onDeviceReady() {
 
     // Mock device.platform property if not available
@@ -8,6 +11,9 @@ function onDeviceReady() {
     }
 
     handleExternalURLs();
+    
+    
+    initPushwoosh();
 }
 
 function handleExternalURLs() {
@@ -31,4 +37,36 @@ function handleExternalURLs() {
     else {
         // Leave standard behaviour
     }
+}
+
+
+function initPushwoosh()
+{
+    var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+ 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_NUMBER", pw_appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "AIzaSyD1wfhWWSovRCKzmS4BgCADcqxWmDvfmkg", pw_appid : "8BF22-5CE01" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
 }
