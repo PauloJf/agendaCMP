9dd89059293d30c9cf7850af50bf39f0f7ab5ca9
PauloJf <PauloJf@users.noreply.github.com>
2015-10-14 11:45:26 +0100

    Update phonegap.js

--- phonegap.js
+++ phonegap.js
@@ -64,9 +64,11 @@ function initPushwoosh()
         function(status) {
             var pushToken = status;
             console.warn('push token: ' + pushToken);
+            alert('push token: ' + pushToken)
         },
         function(status) {
             console.warn(JSON.stringify(['failed to register ', status]));
+            alert(JSON.stringify(['failed to register ', status]))
         }
     );
 }
