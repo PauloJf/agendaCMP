6732b88651e0f697227913670f628185e008efd1
PauloJf <paulofernandes@Paulos-MacBook-Pro.local>
2015-11-02 15:22:17 +0000

    push

--- js/PushwooshiOS.js
+++ js/PushwooshiOS.js
@@ -39,7 +39,7 @@ function registerPushwooshIOS() {
 	);
 
 	//initialize the plugin
-    pushNotification.onDeviceReady({pw_appid:"539E9-AB8AE"});
+    pushNotification.onDeviceReady({pw_appid:"8BF22-5CE01"});
 
 	//register for pushes
 	pushNotification.registerDevice(
